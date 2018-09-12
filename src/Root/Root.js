44d689abdbba9d37d873eeff75dcc7c824f7683b
artur staliarou <artur.staliarou@wolterskluwer.com>
2018-09-12 17:49:47 +0300

    -

--- src/Root/Root.js
+++ src/Root/Root.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './Root.css';
 
-import Root from './Blocks/Top/Top';
+import Top from './Blocks/Top/Top';
+import Right from './Blocks/Right/Right';
+import Main from './Blocks/Main/Main';
 
 class Root extends Component {
   render() {
     return (
       <div className="Root">
         Hello world_bla bla!!!
-      </div>
+      </div>      
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root/Root';
+import Main from './Root/Blocks/Main/Main';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Root />,
-    document.getElementById('root')
+    document.getElementById('root')    
 );
 
 registerServiceWorker();
