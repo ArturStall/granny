3d7a523246a37d8555f509e7738f2257b3e95503
artur staliarou <artur.staliarou@wolterskluwer.com>
2018-09-12 17:51:11 +0300

    -

---
+++ src/Root/Blocks/Right/Right.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Right.css';
+
+class Right extends Component {
+  render() {
+    return (
+      <div className="Right">
+        Right!        
+      </div>
+    );
+  }
+}
+
+export default Right;
